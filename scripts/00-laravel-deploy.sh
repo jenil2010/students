cbae64c4d372f404870c01faafb54dbbf451b8cb
jenil2010 <jenil.binstellar@gmail.com>
2024-10-15 17:26:56 +0530

    doc

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,8 +3,8 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
+# echo "generating application key..."
+# php artisan key:generate --show
 
 echo "Caching config..."
 php artisan config:cache
@@ -15,4 +15,3 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
-php artisan dd:seed
